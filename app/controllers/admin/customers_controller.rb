ca0b5c0d26f7ccb8725a230ae166acb85a2bda63
EC2 Default User <ec2-user@ip-172-31-43-94.ap-northeast-1.compute.internal>
2023-09-21 08:06:35 +0000

    ジャンル機能作成中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,25 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customers_path #ユーザー一覧ページへリダイレクト。
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :name, :is_deleted)
   end
 end
